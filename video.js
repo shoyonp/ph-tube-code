2885650b20bea4471d51810785e23a840dedd26a
shoyonp <shoyonkumar143@gmail.com>
2024-10-03 16:07:49 +0600

    added video cards with dynamically thumbnail

--- video.js
+++ video.js
@@ -9,6 +9,56 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error));
 };
+const loadVideos = () => {
+  //    fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+const cardDemo = {
+    "category_id": "1001",
+    "video_id": "aaad",
+    "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
+    "title": "Smells Like Teen Spirit",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
+            "profile_name": "Oliver Harris",
+            "verified": true
+        }
+    ],
+    "others": {
+        "views": "5.4K",
+        "posted_date": "1672656000"
+    },
+    "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
+}
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoContainer.append(card);
+  });
+};
 
 // {
 //     "category_id": "1001",
@@ -32,3 +82,4 @@ const displayCategories = (categories) => {
 };
 
 loadCategories();
+loadVideos();
