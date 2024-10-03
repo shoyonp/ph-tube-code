0f49a1650393bbb9eed700b50251d0e3a3571aad
shoyonp <shoyonkumar143@gmail.com>
2024-10-03 17:17:04 +0600

    added verified badge

---
+++ test.js
@@ -0,0 +1,10 @@
+const isVerified = true;
+
+// if(isVerified === true){
+//     console.log('user is verified');
+// }
+// else{
+//     console.log('user is not verified');
+// }
+
+console.log(`${isVerified === true ? "user is verifide" : "user is not verified"}`);
