e125a4da6a9ccd392e8b6d56fd15d72578a0dc1c
shoyonp <shoyonkumar143@gmail.com>
2024-10-03 16:45:40 +0600

    added profile name ,video title

--- video.js
+++ video.js
@@ -17,24 +17,24 @@ const loadVideos = () => {
     .catch((error) => console.log(error));
 };
 
-const cardDemo = {
-    "category_id": "1001",
-    "video_id": "aaad",
-    "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
-    "title": "Smells Like Teen Spirit",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
-            "profile_name": "Oliver Harris",
-            "verified": true
-        }
-    ],
-    "others": {
-        "views": "5.4K",
-        "posted_date": "1672656000"
-    },
-    "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
-}
+// const cardDemo = {
+//     "category_id": "1001",
+//     "video_id": "aaad",
+//     "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
+//     "title": "Smells Like Teen Spirit",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
+//             "profile_name": "Oliver Harris",
+//             "verified": true
+//         }
+//     ],
+//     "others": {
+//         "views": "5.4K",
+//         "posted_date": "1672656000"
+//     },
+//     "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
+// }
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
@@ -43,17 +43,24 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure>
+    <figure class="h-[200px]">
     <img
       src=${video.thumbnail}
+      class="h-full w-full object-cover"
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex gap-2">
+   <div>
+        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}"/>
+   </div>
+   <div>
+   <h2 class="font-bold">${video.title}</h2>
+    <div class="flex items-center gap-2">
+     <p class="text-gray-400">${video.authors[0].profile_name}</p>
+     <img class="w-5" src=""/>
     </div>
+   <p></p>
+   </div>
   </div>
     `;
     videoContainer.append(card);
