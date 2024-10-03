33c8b912cb4e22869b9c6a7bdbd69a7fef71d745
shoyonp <shoyonkumar143@gmail.com>
2024-10-03 20:51:52 +0600

    all done and added search dynamic

--- video.js
+++ video.js
@@ -24,9 +24,9 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error));
 };
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
   //    fetch the data
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json())
     .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error));
@@ -166,6 +166,10 @@ const displayCategories = (categories) => {
     categoryContainer.append(buttonContainer);
   });
 };
+
+document.getElementById('search-input').addEventListener("keyup",(e)=>{
+    loadVideos(e.target.value);
+});
 
 loadCategories();
 loadVideos();
